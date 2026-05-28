a7ed96231022d10de139bc9a048a400bdcbe9e73
gadila-10 <gadilanagasree@gmail.com>
2026-05-28 11:09:26 +0530

    Add files via upload

---
+++ quiz_application/Question.java
@@ -0,0 +1,21 @@
+package com.quizapp;
+
+public class Question {
+    private String questionText;
+    private String[] options;
+    private int correctOptionIndex;
+
+    public Question(String questionText, String[] options, int correctOptionIndex) {
+        this.questionText = questionText;
+        this.options = options;
+        this.correctOptionIndex = correctOptionIndex;
+    }
+
+    public String getQuestionText() { return questionText; }
+    public String[] getOptions() { return options; }
+    public int getCorrectOptionIndex() { return correctOptionIndex; }
+
+    public boolean isCorrectAnswer(int chosenOption) {
+        return chosenOption == correctOptionIndex;
+    }
+}
